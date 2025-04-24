884b670bef942d053ce16a10d2ebd3c8b5316d3d
izar26 <nizar.nizar.nizar.26@gmail.com>
2025-04-24 09:37:05 +0700

    first commit

---
+++ pages/profil.php
@@ -0,0 +1,26 @@
+<?php include '../partials/header.php'; ?>
+<main class="container mt-5">
+    <h1 class="mb-4">Profil Kantor Imigrasi</h1>
+    <h4>Sejarah</h4>
+    <p>Kantor Imigrasi berdiri sejak tahun 2019 dengan tujuan memberikan layanan keimigrasian kepada masyarakat.</p>
+    
+    <h4>Visi & Misi</h4>
+    <ul>
+        <li><strong>Visi:</strong> Kementerian Hukum dan Hak Asasi Manusia yang Andal, Profesional, Inovatif, dan Berintegritas dalam pelayanan kepada Presiden dan Wakil Presiden untuk mewujudkan Visi dan Misi Presiden dan Wakil Presiden Indonesia Maju yang Berdaulat, Mandiri, dan Berkepribadian berlandaskan Gotong Royong</li>
+        <li><strong>Misi:</strong> Membentuk peraturan Perundang-Undangan yang berkualitas dan melindungi kepentingan nasional;
+Menyelenggarakan pelayanan publik di bidang hukum yang berkualitas;
+Mendukung penegakan Hukum di Bidang Kekayaan Intelektual, Keimigrasian, Administrasi Hukum Umum, dan Pemasyarakatan yang bebas dari korupsi, bermartabat dan terpercaya;
+Melaksanakan penghormatan, perlindungan dan pemenuhan Hak Asasi Manusia yang berkelanjutan;
+Melaksanakan peningkatan Kesadaran Hukum Masyarakat;
+Ikut serta menjaga stabilitas keamanan melalui peran Keimigrasian dan Pemasyarakatan; serta
+Melaksanakan tata laksana pemerintahan yang baik melalui Reformasi Birokrasi dan Kelembagaan</li>
+    </ul>
+
+    <h4>Struktur Organisasi</h4>
+    <img src="../assets/img/struktur.jpg" class="img-fluid mb-3 rounded" alt="Struktur Organisasi">
+
+    <h4>Tugas dan Fungsi</h4>
+    <p>Tugas Pokok: Mengatur lalu lintas orang, melayani dokumen perjalanan dan izin tinggal, menegakkan hukum keimigrasian, serta melakukan intelijen keimigrasian.</p>
+    <p>Fungsi Utama: Pelayanan dokumen perjalanan dan izin tinggal, pengawasan dan intelijen keimigrasian, penindakan pelanggaran, pengelolaan sistem TI keimigrasian, penyampaian informasi publik, serta administrasi internal dan pelaporan.</p>
+</main>
+<?php include '../partials/footer.php'; ?>
